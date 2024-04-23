d9693bb0d967d9e4e9248d9316286e3d6693955f
DELESON <2271755156@qq.com>
2024-04-23 19:32:03 +0800

    change config

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Deleson",
-  base: "/git_study/",
+  base: "/git_study",
   description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
